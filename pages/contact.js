06cfda5132efc546e5960388ad17e19a00016c15
flyboy85749 <freedmouse@gmail.com>
2022-03-23 11:26:57 -0700

    form working

--- pages/contact.js
+++ pages/contact.js
@@ -6,46 +6,42 @@ const Contact = () => {
       <h3 className="mb-8 border-b pb-4 text-xl font-semibold">
         Leave a Reply
       </h3>
-      <form action="https://formsubmit.co/bill@christianwebdeveloper.com" method="POST">
-      <div className="mb-4 grid grid-cols-1 gap-4 lg:grid-cols-2">
-        <input
-          type="text"
-          
-          className="w-full rounded-lg bg-gray-100 py-2 px-4 text-gray-700 outline-none focus:ring-2 focus:ring-gray-200"
-          placeholder="Name"
-          name="name"
-        />
-        <input
-          type="email"
-          
-          className="w-full rounded-lg bg-gray-100 py-2 px-4 text-gray-700 outline-none focus:ring-2 focus:ring-gray-200"
-          placeholder="Email"
-          name="email"
-        />
-      </div>
-      <div className="mb-4 grid grid-cols-1 gap-4">
-        <textarea
-          
-          
-          className="h-40 w-full rounded-lg bg-gray-100 p-4 text-gray-700 outline-none focus:ring-2 focus:ring-gray-200"
-          name="message"
-          placeholder="Message"
-        />
-      </div>
-      <div className="mt-8">
-        <button
-          type="submit"
-          
-          className="ease inline-block cursor-pointer rounded-full bg-pink-600 px-8 py-3 text-lg font-medium text-white transition duration-500 hover:bg-indigo-900"
-        >
-          Submit Message
-        </button>
-        
-      </div>
+      <form
+        action="https://formsubmit.co/bill@christianwebdeveloper.com"
+        method="POST"
+      >
+        <div className="mb-4 grid grid-cols-1 gap-4 lg:grid-cols-2">
+          <input
+            type="text"
+            className="w-full rounded-lg bg-gray-100 py-2 px-4 text-gray-700 outline-none focus:ring-2 focus:ring-gray-200"
+            placeholder="Name"
+            name="name"
+          />
+          <input
+            type="email"
+            className="w-full rounded-lg bg-gray-100 py-2 px-4 text-gray-700 outline-none focus:ring-2 focus:ring-gray-200"
+            placeholder="Email"
+            name="email"
+          />
+        </div>
+        <div className="mb-4 grid grid-cols-1 gap-4">
+          <textarea
+            className="h-40 w-full rounded-lg bg-gray-100 p-4 text-gray-700 outline-none focus:ring-2 focus:ring-gray-200"
+            name="message"
+            placeholder="Message"
+          />
+        </div>
+        <div className="mt-8">
+          <button
+            type="submit"
+            className="ease inline-block cursor-pointer rounded-full bg-pink-600 px-8 py-3 text-lg font-medium text-white transition duration-500 hover:bg-indigo-900"
+          >
+            Submit Message
+          </button>
+        </div>
       </form>
     </div>
   )
 }
 
-
-export default Contact;
+export default Contact
